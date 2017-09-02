f4cfa746909b450a48b3feb7da789a5f425967d1
Refael-Moblin <Refael@MOBLIN.com>
2017-09-02 16:32:04 +0300

    added Fan model crud operations support

---
+++ ShauliBlog/Models/Fan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ShauliBlog.Models
+{
+    public class Fan
+    {
+        [Key]
+        [Column(Order = 1)]
+        public String FirstName { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public String LastName { get; set; }
+
+        public bool Gender { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public int Seniority { get; set; }
+    }
+}
