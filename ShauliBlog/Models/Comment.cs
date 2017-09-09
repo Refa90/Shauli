56bceed361f85c2bcbdde74e99c3145255abd5d8
Refael-Moblin <Refael@MOBLIN.com>
2017-09-09 10:19:08 +0300

    added new models

---
+++ ShauliBlog/Models/Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace ShauliBlog.Models
+{
+    public class Comment : Essay
+    {
+        public int PostId { get; set; }
+
+        public virtual Post Post { get; set; }
+    }
+}
---
+++ ShauliBlog/Models/Essay.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShauliBlog.Models
+{
+    public abstract class Essay
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Headline { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string AuthorWebsiteAddress { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
